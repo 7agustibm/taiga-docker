7d6f2d4eda7eec9a50abbeeaa0b437c6ca77d333
Agustí Becerra Milà <contact@agustibm.com>
2018-01-18 11:27:53 +0100

    Fix for conf.json

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@
 docker-compose build
 docker-compose up -d
 docker-compose exec taigaback /usr/src/app/taiga-back/regenerate.sh
-docker-compose exec taigafront sed -i 's/:\/\/\/api\/v1\//api\/v1\//g' /taiga/conf.json
+docker-compose exec taigafront sed -i 's/:\/\/\/api\/v1\//\/api\/v1\//g' /taiga/conf.json
